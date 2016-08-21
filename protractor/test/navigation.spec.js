b672f607ef5ba15a822e760d75155f5139a98f12
Chris Carter <chris@ccarteronline.com>
2016-08-21 10:46:13 -0400

    test(e2e): Add more to page navigation tests
    Write short tests that confirm navigation and sidebar can bring up the right page. Other tests can continue with page objects for reusable elements to be used during testing interactions.

--- protractor/test/navigation.spec.js
+++ protractor/test/navigation.spec.js
@@ -1,14 +1,14 @@
 (function() {
     'use strict';
-    
-    var q = require('q');
+
     describe('Page Navigation: Navigation Bar elements', function() {
 
         var navigationLinks = ['AUTHORS', 'TEACHERS', 'JSON', '', '', ''];
+        var mainPageUrl = 'http://localhost:7803/';
 
         it('should navigate to the homepage', function() {
             // Set the page URL
-            var mainPageUrl = 'http://localhost:7803/';
+
 
             // Visit the Page
             browser.get(mainPageUrl);
@@ -33,15 +33,70 @@
             });
         });
 
-        it('should open the author page', function () {
+        it('should open the sideBar for: Authors', function () {
             // Could be extacted out as a page object
             var btnLinkTitle = 'AUTHORS';
             var index = navigationLinks.indexOf(btnLinkTitle);
-            var aboutBtn = element.all(by.css('[ng-click="vm.navigateTo(btn, false)"]')).get(index);
+            var authorBtn = element.all(by.css('[ng-click="vm.navigateTo(btn, false)"]')).get(index);
+            var authorToClick = element.all(by.css('[ng-click="showTab(item.type, item.uuid)"]')).get(1);
 
-            aboutBtn.click();
-            browser.pause();
+            // Open the Side bar
+            authorBtn.click();
+
+            expect(authorToClick.getText()).toContain('Elizabeth Jones');
+
+            // Click on the Author to go to their page
+            authorToClick.click();
         });
+
+        it('should have visited an AUTHORS page', function () {
+            browser.getCurrentUrl().then(function (url) {
+                expect(url).toContain('/author?uuid=');
+            });
+        });
+
+        it('should open the sideBar for: Teachers', function () {
+            var btnLinkTitle = 'TEACHERS';
+            var index = navigationLinks.indexOf(btnLinkTitle);
+            var teacherBtn = element.all(by.css('[ng-click="vm.navigateTo(btn, false)"]')).get(index);
+            var teacherToClick = element.all(by.css('[ng-click="showTab(item.type, item.uuid)"]')).get(1);
+
+            teacherBtn.click();
+
+            expect(teacherToClick.getText()).toContain('Janice, Thompson');
+
+            teacherToClick.click();
+        });
+
+        it('should have visited a TEACHERS page', function () {
+            browser.getCurrentUrl().then(function (url) {
+                expect(url).toContain('/teacher?uuid=');
+            });
+        });
+
+        it('should have visited the JSON page', function () {
+            var btnLinkTitle = 'JSON';
+            var index = navigationLinks.indexOf(btnLinkTitle);
+            var jsonBtn = element.all(by.css('[ng-click="vm.navigateTo(btn, false)"]')).get(index);
+
+            jsonBtn.click();
+
+            browser.getCurrentUrl().then(function (url) {
+                expect(url).toContain('/json');
+            });
+        });
+
+        it('should go to homescreen when logo text is clicked', function () {
+            var logoText = $('.NavigationBar span a');
+
+            logoText.click();
+
+            browser.getCurrentUrl().then(function (url) {
+                expect(url).toContain('mainPageUrl');
+            });
+        });
+
+
     });
 
 }());
